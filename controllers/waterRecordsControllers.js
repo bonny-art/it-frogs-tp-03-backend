62d6fd32910973705ef62d9d222a3613301078eb
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2024-03-28 21:14:14 +0200

    currentday

--- controllers/waterRecordsControllers.js
+++ controllers/waterRecordsControllers.js
@@ -1,3 +1,20 @@
-import HttpError from "../helpers/HttpError.js";
+const { waterRecordServices } = require("../services/waterRecordServices");
 
-import * as ContactsService from "../services/waterRecordServices.js";
+const getCurrentDay = async (req, res) => {
+    try {
+      const { entryDate } = req.params;
+      const {userId} = req.params;
+      const dailyWater = await waterRecordServices.getCurrentDay(entryDate, userId);
+      
+      if (!dailyWater) {
+        return res.json({ waterIntakes: [], consumedWaterPercentage: 0 });
+      }
+      
+      res.json(dailyWater);
+    } catch (error) {
+      console.error("An error occurred:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
+
+  module.exports = {getCurrentDay};
