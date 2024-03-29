bea7fbd8fd46f0b43dc985a20ed013eaf9cac06c
Oleksiy Pokormyakho <116438996+OleksiyPok@users.noreply.github.com>
2024-03-29 22:48:35 +0100

    swagger has been updated

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 
-// import routes from "./routes/index.js";
+import routes from "./routes/index.js";
 
 import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "./swagger_templ.json" assert { type: "json" };
-// import swaggerDocument from "./swagger.json" assert { type: "json" };
+import swaggerDocument from "./swagger.json" assert { type: "json" };
 
 const app = express();
 
@@ -14,18 +13,18 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-// app.use("/api", routes);
+app.use("/api", routes);
 
 app.use("/api-docs", swaggerUi.serve);
 app.get("/api-docs", swaggerUi.setup(swaggerDocument));
 
-// app.use((_, res) => {
-//   res.status(404).json({ message: "Route not found" });
-// });
+app.use((_, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
 
-// app.use((err, req, res, next) => {
-//   const { status = 500, message = "Server error" } = err;
-//   res.status(status).json({ message });
-// });
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
 
 export default app;
