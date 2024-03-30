1d0f1887e402a2ad52703cfeb6c4ec16e0804f9e
Oleksiy Pokormyakho <116438996+OleksiyPok@users.noreply.github.com>
2024-03-30 11:35:43 +0100

    apiDocsRoutes.js has been added

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ import cors from "cors";
 
 import routes from "./routes/index.js";
 
-import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "./swagger.json" assert { type: "json" };
-
 const app = express();
 
 app.use(morgan("tiny"));
@@ -14,9 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", routes);
-
-app.use("/api-docs", swaggerUi.serve);
-app.get("/api-docs", swaggerUi.setup(swaggerDocument));
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
