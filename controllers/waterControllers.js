22d55a4ef3bf536668d839f23100b8a481786b0e
bonny-art <svitlana.otenko@gmail.com>
2024-04-01 03:44:26 +0300

    adds a controller and service to change the water intake (time and/or volume)

--- controllers/waterControllers.js
+++ controllers/waterControllers.js
@@ -1,6 +1,10 @@
 import HttpError from "../helpers/HttpError.js";
 import * as waterServices from "../services/waterServices.js";
 
+// Function for adding a new intake of water
+// If there is no record for today, it will create it and add a new intake
+// If there is a record, it will add new intake to the existing record
+
 export const addWaterIntakeRecord = async (req, res, next) => {
   const { data, ml } = req.body;
 
@@ -49,12 +53,120 @@ export const addWaterIntakeRecord = async (req, res, next) => {
   }
 };
 
+// Function of changing the water intake:
+// amount of water and/or time of intake
+// If there is no record on this day, an error will be returned
+// If there is no water intake with this id on this day, an error will be returned
+
 export const updateWaterIntakeRecord = async (req, res, next) => {
+  const { data, ml } = req.body;
+  const { waterRecordId } = req.params;
+
+  const entryDate = new Date(`${data}:00.000Z`);
+  entryDate.setUTCHours(0, 0, 0, 0);
+
+  const consumedAt = new Date(`${data}:00.000Z`);
+
+  const { _id } = req.user;
+
+  const params = {
+    entryDate,
+    userId: _id,
+  };
+
   try {
-    res.send("Water record was updated.");
+    const dailyWater = await waterServices.findWaterRecord(params);
+
+    if (!dailyWater) {
+      throw HttpError(404, "There are no records on this day");
+    }
+
+    const { waterIntakes } = dailyWater;
+
+    const waterIntake = waterIntakes.find(
+      ({ _id }) => String(_id) === waterRecordId
+    );
+
+    if (!waterIntake) {
+      throw HttpError(404, "There is no such record of water intake");
+    }
+
+    const consumedWater = dailyWater.consumedWater - waterIntake.ml + ml;
+
+    const waterPercentage = (consumedWater / dailyWater.dailyWaterGoal) * 100;
+
+    const payload = {
+      _id: waterIntake._id,
+      ml,
+      consumedAt,
+      waterPercentage,
+      consumedWater,
+    };
+
+    const newDailyWater = await waterServices.updateWaterIntake(
+      params,
+      payload
+    );
+
+    res.send({ newDailyWater });
   } catch (error) {
     next(error);
   }
 };
 
-export const removeWaterIntakeRecord = async (req, res, next) => {};
+// Function of deleting water intake
+// If there is no record on this day, an error will be returned
+// If there is no water intake with this id on this day, it will return an error
+
+export const removeWaterIntakeRecord = async (req, res, next) => {
+  const { data } = req.body;
+  const { waterRecordId } = req.params;
+
+  const entryDate = new Date(`${data}:00.000Z`);
+  entryDate.setUTCHours(0, 0, 0, 0);
+
+  const { _id } = req.user;
+
+  const params = {
+    entryDate,
+    userId: _id,
+  };
+
+  try {
+    const dailyWater = await waterServices.findWaterRecord(params);
+
+    if (!dailyWater) {
+      throw HttpError(404, "There are no records on this day");
+    }
+
+    const { waterIntakes } = dailyWater;
+
+    const waterIntake = waterIntakes.find(
+      ({ _id }) => String(_id) === waterRecordId
+    );
+
+    if (!waterIntake) {
+      throw HttpError(404, "There is no such record of water intake");
+    }
+
+    const waterPercentage =
+      ((dailyWater.consumedWater - waterIntake.ml) /
+        dailyWater.dailyWaterGoal) *
+      100;
+
+    const payload = {
+      _id: waterIntake._id,
+      ml: waterIntake.ml,
+      waterPercentage,
+    };
+
+    const newDailyWater = await waterServices.removeWaterIntake(
+      params,
+      payload
+    );
+
+    res.send({ newDailyWater });
+  } catch (error) {
+    next(error);
+  }
+};
